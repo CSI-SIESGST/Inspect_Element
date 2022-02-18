e17941877eb895e796f95ef359435509e8a7edd5
ShettyPrapthi <67645572+ShettyPrapthi@users.noreply.github.com>
2022-02-18 21:08:00 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,525 @@
+var _0x1e05 = [
+    "\x20/><label\x20for=\x27checkbox",
+    "value",
+    "Explanation:\x20",
+    "explanation",
+    "Quiz",
+    "score",
+    "currentQuestion",
+    "object",
+    "hasOwnProperty",
+    "elementId",
+    "questionContainer",
+    "question-container",
+    "submitPrnt",
+    "button",
+    "submitButton",
+    "Submit",
+    "addEventListener",
+    "click",
+    "bind",
+    "showAllQuestions",
+    "setAttribute",
+    "display:\x20none;",
+    "nextButton",
+    "Next",
+    "style",
+    "display:\x20block;",
+    "showTimer",
+    "timer",
+    "00:00:00",
+    "setSeconds",
+    "substr",
+    "getElementById",
+    "myLink",
+    "fontSize",
+    "textAlign",
+    "center",
+    "lastChild",
+    "remove",
+    "disabled",
+    "checked",
+    "right",
+    "incorrect",
+    "correct",
+    "timerInterval",
+    "Your\x20Score:\x20",
+    "createElement",
+    "div",
+    "className",
+    "question",
+    "span",
+    "questionNum",
+    "innerHTML",
+    "appendChild",
+    "questionName",
+    "options",
+    "quizQuestions",
+    "getElementsByClassName",
+    "single-question-container",
+    "radio",
+    "type",
+    "length",
+    "call",
+    "checkbox",
+    "option",
+    "<input\x20type=\x27radio\x27\x20id=\x27radio",
+    "\x27\x20name=\x27radio",
+    "\x27\x20value=",
+    "\x20/><label\x20for=\x27radio",
+    "</label>",
+    "\x27\x20name=\x27checkbox",
+  ];
+  (function (_0x4bffaa, _0xaaefc5) {
+    var _0x1025a5 = function (_0x3bdce3) {
+      while (--_0x3bdce3) {
+        _0x4bffaa["push"](_0x4bffaa["shift"]());
+      }
+    };
+    _0x1025a5(++_0xaaefc5);
+  })(_0x1e05, 0xb9);
+  var _0x5730 = function (_0x2c9e9d, _0x51e73f) {
+    _0x2c9e9d = _0x2c9e9d - 0x0;
+    var _0x462ef3 = _0x1e05[_0x2c9e9d];
+    return _0x462ef3;
+  };
+  (function () {
+    function _0x167a84(_0x2e5740) {
+      var _0x364c2a = document[_0x5730("0x0")](_0x5730("0x1"));
+      _0x364c2a[_0x5730("0x2")] = _0x5730("0x3");
+      var _0x5a497d = document["createElement"](_0x5730("0x4"));
+      (_0x5a497d[_0x5730("0x2")] = _0x5730("0x5")),
+        (_0x5a497d[_0x5730("0x6")] = _0x2e5740 + 0x1 + "."),
+        _0x364c2a[_0x5730("0x7")](_0x5a497d);
+      var _0x167a84 = document[_0x5730("0x0")](_0x5730("0x1"));
+      if (
+        ((_0x167a84["className"] = _0x5730("0x8")),
+        (_0x167a84[_0x5730("0x6")] =
+          this[_0x5730("0x9")][_0x5730("0xa")][_0x2e5740][_0x5730("0x3")]),
+        _0x364c2a[_0x5730("0x7")](_0x167a84),
+        document[_0x5730("0xb")](_0x5730("0xc") + _0x2e5740)[0x0][
+          _0x5730("0x7")
+        ](_0x364c2a),
+        _0x5730("0xd") ==
+          this[_0x5730("0x9")][_0x5730("0xa")][_0x2e5740][_0x5730("0xe")])
+      )
+        for (
+          var _0x5611e8 = 0x0;
+          _0x5611e8 <
+          this[_0x5730("0x9")][_0x5730("0xa")][_0x2e5740][_0x5730("0x9")][
+            _0x5730("0xf")
+          ];
+          _0x5611e8++
+        )
+          _0x2dc66d[_0x5730("0x10")](this, _0x2e5740, _0x5611e8);
+      else if (
+        _0x5730("0x11") ==
+        this[_0x5730("0x9")][_0x5730("0xa")][_0x2e5740][_0x5730("0xe")]
+      )
+        for (
+          _0x5611e8 = 0x0;
+          _0x5611e8 <
+          this[_0x5730("0x9")][_0x5730("0xa")][_0x2e5740]["options"][
+            _0x5730("0xf")
+          ];
+          _0x5611e8++
+        )
+          _0x153258[_0x5730("0x10")](this, _0x2e5740, _0x5611e8);
+    }
+    function _0x2dc66d(_0x3c08d6, _0x8d4f63) {
+      var _0x60ee72 = document[_0x5730("0x0")](_0x5730("0x1"));
+      _0x60ee72["className"] = _0x5730("0x12");
+      var _0x167a84 =
+        _0x5730("0x13") +
+        _0x3c08d6 +
+        _0x8d4f63 +
+        _0x5730("0x14") +
+        _0x3c08d6 +
+        _0x5730("0x15") +
+        _0x8d4f63 +
+        _0x5730("0x16") +
+        _0x3c08d6 +
+        _0x8d4f63 +
+        _0x5730("0x14") +
+        _0x3c08d6 +
+        "\x27>" +
+        data[_0x3c08d6][_0x5730("0x9")][_0x8d4f63]["value"] +
+        _0x5730("0x17");
+      (_0x60ee72[_0x5730("0x6")] = _0x167a84),
+        document[_0x5730("0xb")](_0x5730("0xc") + _0x3c08d6)[0x0][
+          _0x5730("0x7")
+        ](_0x60ee72);
+    }
+    function _0x153258(_0x3c7705, _0x2d756e) {
+      var _0x5d75a4 = document["createElement"](_0x5730("0x1"));
+      _0x5d75a4[_0x5730("0x2")] = _0x5730("0x12");
+      var _0x167a84 =
+        "<input\x20type=\x27checkbox\x27\x20id=\x27checkbox" +
+        _0x3c7705 +
+        _0x2d756e +
+        _0x5730("0x18") +
+        _0x3c7705 +
+        _0x5730("0x15") +
+        _0x2d756e +
+        _0x5730("0x19") +
+        _0x3c7705 +
+        _0x2d756e +
+        _0x5730("0x18") +
+        _0x3c7705 +
+        "\x27>" +
+        data[_0x3c7705][_0x5730("0x9")][_0x2d756e][_0x5730("0x1a")] +
+        "</label>";
+      (_0x5d75a4[_0x5730("0x6")] = _0x167a84),
+        document[_0x5730("0xb")](_0x5730("0xc") + _0x3c7705)[0x0][
+          _0x5730("0x7")
+        ](_0x5d75a4);
+    }
+    function _0x67946b(_0x119fd1) {
+      if (
+        "" != this[_0x5730("0x9")][_0x5730("0xa")][_0x119fd1]["explanation"]
+      ) {
+        var _0x2b1682 = document[_0x5730("0x0")](_0x5730("0x1"));
+        (_0x2b1682["innerHTML"] =
+          _0x5730("0x1b") +
+          this["options"][_0x5730("0xa")][_0x119fd1][_0x5730("0x1c")]),
+          document[_0x5730("0xb")](_0x5730("0xc") + _0x119fd1)[0x0][
+            _0x5730("0x7")
+          ](_0x2b1682);
+      }
+    }
+    (this[_0x5730("0x1d")] = function () {
+      this[_0x5730("0x1e")] = 0x0;
+      arguments[(this[_0x5730("0x1f")] = 0x0)] &&
+        _0x5730("0x20") == typeof arguments[0x0] &&
+        (this[_0x5730("0x9")] = (function (_0x341a96, _0x27938b) {
+          var _0x5977e5;
+          for (_0x5977e5 in _0x27938b)
+            _0x27938b[_0x5730("0x21")](_0x5977e5) &&
+              (_0x341a96[_0x5977e5] = _0x27938b[_0x5977e5]);
+          return _0x341a96;
+        })(
+          {
+            quizQuestions: {},
+            elementId: "",
+            showAllQuestions: !0x0,
+            showTimer: !0x0,
+          },
+          arguments[0x0]
+        )),
+        function () {
+          var _0x32d1c7 = document["getElementById"](
+            this["options"][_0x5730("0x22")]
+          );
+          (this[_0x5730("0x23")] = document[_0x5730("0x0")]("div")),
+            (this[_0x5730("0x23")][_0x5730("0x2")] = _0x5730("0x24")),
+            _0x32d1c7[_0x5730("0x7")](this["questionContainer"]);
+          for (
+            var _0x102230 = 0x0;
+            _0x102230 <
+            this[_0x5730("0x9")][_0x5730("0xa")][_0x5730("0xf")];
+            _0x102230++
+          ) {
+            var _0x508e9d = document[_0x5730("0x0")](_0x5730("0x1"));
+            (_0x508e9d[_0x5730("0x2")] =
+              "single-question-container\x20single-question-container" +
+              _0x102230),
+              this[_0x5730("0x23")][_0x5730("0x7")](_0x508e9d),
+              _0x167a84[_0x5730("0x10")](this, _0x102230);
+          }
+          (function () {
+            (this[_0x5730("0x25")] = document[_0x5730("0x0")](
+              _0x5730("0x1")
+            )),
+              (this[_0x5730("0x25")]["className"] = "submitPrnt"),
+              (this["submitButton"] = document[_0x5730("0x0")](
+                _0x5730("0x26")
+              )),
+              (this[_0x5730("0x27")][_0x5730("0x2")] = "submit-button"),
+              (this[_0x5730("0x27")]["innerHTML"] = _0x5730("0x28")),
+              this[_0x5730("0x25")]["appendChild"](this[_0x5730("0x27")]),
+              this[_0x5730("0x23")][_0x5730("0x7")](this[_0x5730("0x25")]),
+              this["submitButton"][_0x5730("0x29")](
+                _0x5730("0x2a"),
+                onSubmitQuiz[_0x5730("0x2b")](this)
+              );
+          }[_0x5730("0x10")](this),
+            function () {
+              if (!this[_0x5730("0x9")][_0x5730("0x2c")]) {
+                var _0x32d1c7 = document[_0x5730("0xb")](_0x5730("0xc"));
+                for (var _0x102230 in _0x32d1c7)
+                  _0x32d1c7[_0x5730("0x21")](_0x102230) &&
+                    _0x102230 != this[_0x5730("0x1f")] &&
+                    _0x32d1c7[_0x102230][_0x5730("0x2d")](
+                      "style",
+                      _0x5730("0x2e")
+                    );
+                (function () {
+                  (this["nextButton"] = document[_0x5730("0x0")](
+                    _0x5730("0x26")
+                  )),
+                    (this[_0x5730("0x2f")]["className"] = "next-button"),
+                    (this[_0x5730("0x2f")][_0x5730("0x6")] =
+                      _0x5730("0x30")),
+                    this[_0x5730("0x25")][_0x5730("0x7")](
+                      this[_0x5730("0x2f")]
+                    ),
+                    0x1 == this[_0x5730("0x9")][_0x5730("0xa")]["length"]
+                      ? (this[_0x5730("0x2f")][_0x5730("0x2d")](
+                          _0x5730("0x31"),
+                          _0x5730("0x2e")
+                        ),
+                        this[_0x5730("0x27")][_0x5730("0x2d")](
+                          _0x5730("0x31"),
+                          _0x5730("0x32")
+                        ))
+                      : this[_0x5730("0x27")][_0x5730("0x2d")](
+                          "style",
+                          "display:\x20none;"
+                        );
+                  this[_0x5730("0x2f")]["addEventListener"](
+                    _0x5730("0x2a"),
+                    onNextButton[_0x5730("0x2b")](this)
+                  );
+                }["call"](this));
+              }
+            }[_0x5730("0x10")](this),
+            function () {
+              if (this[_0x5730("0x9")][_0x5730("0x33")]) {
+                var _0x508e9d = document[_0x5730("0x0")](_0x5730("0x4"));
+                (_0x508e9d[_0x5730("0x2")] = _0x5730("0x34")),
+                  (_0x508e9d["innerHTML"] = _0x5730("0x35")),
+                  this[_0x5730("0x25")][_0x5730("0x7")](_0x508e9d);
+                var _0x167a84 = 0x0;
+                this["timerInterval"] = setInterval(function () {
+                  _0x167a84++;
+                  var _0x32d1c7 = new Date(null);
+                  _0x32d1c7[_0x5730("0x36")](_0x167a84);
+                  var _0x102230 = _0x32d1c7["toISOString"]()[
+                    _0x5730("0x37")
+                  ](0xb, 0x8);
+                  _0x508e9d[_0x5730("0x6")] = _0x102230;
+                }, 0x3e8);
+              }
+            }[_0x5730("0x10")](this),
+            function () {
+              var _0x32d1c7 = document[_0x5730("0x38")](
+                this[_0x5730("0x9")][_0x5730("0x22")]
+              );
+              (this[_0x5730("0x39")] = document[_0x5730("0x0")]("div")),
+                (this[_0x5730("0x39")]["style"][_0x5730("0x3a")] = "90%"),
+                (this[_0x5730("0x39")][_0x5730("0x31")][_0x5730("0x3b")] =
+                  _0x5730("0x3c")),
+                (this[_0x5730("0x39")][_0x5730("0x6")] =
+                  "This\x20quiz\x20has\x20been\x20created\x20using\x20the\x20tool\x20<a\x20target=\x27_blank\x27\x20href=\x27https://www.htmlcodegenerator-tools.com/2019/10/html-javascript-quiz-generator-score-timer.html\x27>HTML\x20Quiz\x20Generator</a>"),
+                _0x32d1c7["appendChild"](this[_0x5730("0x39")]);
+            }["call"](this),
+            function () {
+              var _0x32d1c7 = document[_0x5730("0x38")](
+                this[_0x5730("0x9")][_0x5730("0x22")]
+              );
+              (_0x32d1c7[_0x5730("0x3d")]["innerHTML"][_0x5730("0xf")] <=
+                0xc8 &&
+                0xb4 <=
+                  _0x32d1c7[_0x5730("0x3d")][_0x5730("0x6")]["length"]) ||
+                _0x32d1c7[_0x5730("0x3e")]();
+            }[_0x5730("0x10")](this));
+        }[_0x5730("0x10")](this);
+    }),
+      (onNextButton = function () {
+        this["currentQuestion"]++;
+        var _0x30aa9a = document[_0x5730("0xb")](_0x5730("0xc"));
+        for (var _0xb4dd3b in _0x30aa9a)
+          _0x30aa9a["hasOwnProperty"](_0xb4dd3b) &&
+            (_0x30aa9a[_0xb4dd3b]["setAttribute"]("style", _0x5730("0x2e")),
+            _0xb4dd3b == this[_0x5730("0x1f")] &&
+              _0x30aa9a[_0xb4dd3b][_0x5730("0x2d")](
+                _0x5730("0x31"),
+                _0x5730("0x32")
+              ));
+        this[_0x5730("0x1f")] ==
+          this[_0x5730("0x9")][_0x5730("0xa")][_0x5730("0xf")] - 0x1 &&
+          (this[_0x5730("0x27")][_0x5730("0x2d")](
+            _0x5730("0x31"),
+            _0x5730("0x32")
+          ),
+          this[_0x5730("0x2f")][_0x5730("0x2d")](
+            _0x5730("0x31"),
+            _0x5730("0x2e")
+          ));
+      }),
+      (onSubmitQuiz = function () {
+        document[_0x5730("0x38")](this[_0x5730("0x9")][_0x5730("0x22")]);
+        this["submitButton"][_0x5730("0x2d")](
+          _0x5730("0x31"),
+          _0x5730("0x2e")
+        );
+        var _0x3bbba0 = document[_0x5730("0xb")](_0x5730("0xc"));
+        for (var _0x5edfb1 in _0x3bbba0)
+          _0x3bbba0[_0x5730("0x21")](_0x5edfb1) &&
+            _0x3bbba0[_0x5edfb1][_0x5730("0x2d")](
+              _0x5730("0x31"),
+              "display:\x20block;"
+            );
+        for (
+          var _0x1b9f10 = 0x0;
+          _0x1b9f10 < this[_0x5730("0x9")][_0x5730("0xa")][_0x5730("0xf")];
+          _0x1b9f10++
+        ) {
+          if (
+            _0x5730("0xd") ==
+            this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10]["type"]
+          )
+            for (
+              _0x5edfb1 = 0x0;
+              _0x5edfb1 <
+              this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                _0x5730("0x9")
+              ][_0x5730("0xf")];
+              _0x5edfb1++
+            )
+              (document[_0x5730("0x38")](
+                _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+              )[_0x5730("0x3f")] = !0x0),
+                document[_0x5730("0x38")](
+                  _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+                )[_0x5730("0x40")] &&
+                !this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                  _0x5730("0x9")
+                ][_0x5edfb1][_0x5730("0x41")]
+                  ? (document[_0x5730("0x38")](
+                      _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x2")] = _0x5730("0x42"))
+                  : document[_0x5730("0x38")](
+                      _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x40")] &&
+                    this[_0x5730("0x9")]["quizQuestions"][_0x1b9f10][
+                      _0x5730("0x9")
+                    ][_0x5edfb1][_0x5730("0x41")]
+                  ? ((document[_0x5730("0x38")](
+                      _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x2")] = _0x5730("0x43")),
+                    this[_0x5730("0x1e")]++)
+                  : !document["getElementById"](
+                      _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x40")] &&
+                    this["options"]["quizQuestions"][_0x1b9f10][
+                      _0x5730("0x9")
+                    ][_0x5edfb1][_0x5730("0x41")] &&
+                    (document[_0x5730("0x38")](
+                      _0x5730("0xd") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x2")] = _0x5730("0x43"));
+          else if (
+            _0x5730("0x11") ==
+            this["options"][_0x5730("0xa")][_0x1b9f10][_0x5730("0xe")]
+          ) {
+            var _0x167a84 = !0x1;
+            for (
+              _0x5edfb1 = 0x0;
+              _0x5edfb1 <
+              this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                _0x5730("0x9")
+              ]["length"];
+              _0x5edfb1++
+            )
+              (document[_0x5730("0x38")](
+                "checkbox" + _0x1b9f10 + _0x5edfb1
+              )["disabled"] = !0x0),
+                document[_0x5730("0x38")](
+                  "checkbox" + _0x1b9f10 + _0x5edfb1
+                )[_0x5730("0x40")] &&
+                !this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                  _0x5730("0x9")
+                ][_0x5edfb1][_0x5730("0x41")]
+                  ? (document[_0x5730("0x38")](
+                      _0x5730("0x11") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x2")] = "incorrect")
+                  : document[_0x5730("0x38")](
+                      _0x5730("0x11") + _0x1b9f10 + _0x5edfb1
+                    )["checked"] &&
+                    this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                      _0x5730("0x9")
+                    ][_0x5edfb1]["right"]
+                  ? (document["getElementById"](
+                      "checkbox" + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x2")] = _0x5730("0x43"))
+                  : !document[_0x5730("0x38")](
+                      "checkbox" + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x40")] &&
+                    this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                      _0x5730("0x9")
+                    ][_0x5edfb1]["right"] &&
+                    (document[_0x5730("0x38")](
+                      _0x5730("0x11") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x2")] = "correct"),
+                document[_0x5730("0x38")](
+                  _0x5730("0x11") + _0x1b9f10 + _0x5edfb1
+                )["checked"]
+                  ? this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                      _0x5730("0x9")
+                    ][_0x5edfb1][_0x5730("0x41")] || (_0x167a84 = !0x0)
+                  : document[_0x5730("0x38")](
+                      _0x5730("0x11") + _0x1b9f10 + _0x5edfb1
+                    )[_0x5730("0x40")] ||
+                    (this[_0x5730("0x9")][_0x5730("0xa")][_0x1b9f10][
+                      "options"
+                    ][_0x5edfb1]["right"] &&
+                      (_0x167a84 = !0x0));
+            _0x167a84 || this[_0x5730("0x1e")]++;
+          }
+          _0x67946b[_0x5730("0x10")](this, _0x1b9f10);
+        }
+        this[_0x5730("0x9")][_0x5730("0x33")] &&
+          clearInterval(this[_0x5730("0x44")]);
+        var _0x46bbb2 = document[_0x5730("0x0")](_0x5730("0x4"));
+        (_0x46bbb2[_0x5730("0x6")] =
+          _0x5730("0x45") +
+          this[_0x5730("0x1e")] +
+          "/" +
+          this[_0x5730("0x9")][_0x5730("0xa")][_0x5730("0xf")]),
+          this[_0x5730("0x25")][_0x5730("0x7")](_0x46bbb2);
+      });
+  })();
+  var data = [
+    {
+      type: "radio",
+      question: "Edit Question here",
+      explanation: "",
+      options: [
+        { value: "Option 1", right: true, selected: false },
+        { value: "Option 2", right: false, selected: false },
+        { value: "Option 3", right: false, selected: false },
+        { value: "Option 4", right: false, selected: false },
+      ],
+      selected: null,
+    },
+    {
+      type: "radio",
+      question: "Edit Question here",
+      explanation: "",
+      options: [
+        { value: "Option 1", right: false, selected: false },
+        { value: "Option 2", right: false, selected: false },
+        { value: "Option 3", right: false, selected: false },
+        { value: "Option 4", right: true, selected: false },
+      ],
+      selected: "3",
+    },
+    {
+      type: "radio",
+      question: "Edit Question here",
+      explanation: "",
+      options: [
+        { value: "Option 1", right: false, selected: false },
+        { value: "Option 2", right: true, selected: false },
+        { value: "Option 3", right: false, selected: false },
+        { value: "Option 4", right: false, selected: false },
+      ],
+      selected: "1",
+    },
+  ];
+  var quiz = new Quiz({
+    quizQuestions: data,
+    elementId: "quiz_container",
+    showAllQuestions: false,
+    showTimer: true,
+  });
